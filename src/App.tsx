953baf175fc090bdbfa834400a3c1d51a3b44c6c
Keg Avakian <kjavakian@outlook.com>
2023-07-05 18:40:44 -0500

    added a better toggle theme button to the nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,21 @@ import Nav from "./components/Nav";
 import { Register, Login } from "./pages/account";
 
 function App() {
-	const [darkTheme, setDarkTheme] = useState(false);
-	const toggleTheme = () => setDarkTheme(!darkTheme);
+  const [darkTheme, setDarkTheme] = useState(false);
+  const toggleTheme = () => setDarkTheme(!darkTheme);
 
-	return (
-		<>
-			<Nav darkTheme={darkTheme} onToggleTheme={toggleTheme} />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/calendar" element={<Calendar />} />
-				<Route path="/today" element={<Today />} />
-				<Route path="/login" element={<Login />} />
-				<Route path="/register" element={<Register />} />
-			</Routes>
-		</>
-	);
+  return (
+    <>
+      <Nav darkMode={darkTheme} onToggleTheme={toggleTheme} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calendar" element={<Calendar />} />
+        <Route path="/today" element={<Today />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
